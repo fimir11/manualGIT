b40e8a5da413b7149385edc85b42a3078a71b35c
alexis <alexis@DESKTOP-IQ0DI3R>
2017-03-14 13:58:37 +0100

    Añadido un método de impresión.

--- Saludo/src/HolaMundo2.java
+++ Saludo/src/HolaMundo2.java
@@ -12,6 +12,7 @@ public class HolaMundo2 {
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo 200");
     }
     
     
